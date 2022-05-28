6025ad6102908a5498c70752d33000a26d511f27
hd159 <81416203+hd159@users.noreply.github.com>
2022-05-28 11:42:41 +0700

    Update setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_PATH = 'postgresql://postgres:postgres@localhost:5432/capstone'
-export AUTH0_DOMAIN = 'dev-x63zmgrg.jp.auth0.com'
-export ALGORITHMS = ['RS256']
-export API_AUDIENCE = 'https://ddc-test.com'
+export DATABASE_PATH='postgresql://postgres:postgres@localhost:5432/capstone'
+export AUTH0_DOMAIN='dev-x63zmgrg.jp.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='https://ddc-test.com'
